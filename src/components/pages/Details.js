cd2e8dad289d0f75824cf267d0d0d8d8400283b3
pedro obando <pedroobando@hotmail.com>
2020-07-13 13:38:34 -0400

    Details - TabContents

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,10 +1,165 @@
-import React from "react";
+import React, { useState } from "react";
+import { InfoConsumer } from "../context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faGooglePlusG,
+  faReddit,
+  faFacebookMessenger,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+
+const iconFacebook = <FontAwesomeIcon icon={faFacebookF} />;
 
 const Details = () => {
+  const [tabSelect, setTabSelect] = useState(1);
+
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setTabSelect({ ...values, [name]: value });
+  // };
+
+  const handleTabSelected = (e) => {
+    const { tabIndex } = e.target;
+    setTabSelect(tabIndex);
+  };
+
   return (
-    <div>
-      <h2>Details...</h2>
-    </div>
+    <InfoConsumer>
+      {(data) => {
+        const {
+          id,
+          headerTitle,
+          headerSubTitle,
+          headerText,
+          img,
+          title,
+          maps,
+          description,
+        } = data.detailInfo;
+
+        return (
+          <React.Fragment>
+            <div className="container-fluid align-items-center">
+              <h1 className="display-1 font-weight-bold">{headerTitle}</h1>
+              <h4 className="display-5">{headerSubTitle}</h4>
+              <p>{headerText}</p>
+              {/* Social Icons */}
+              <div className="container mt-5">
+                <div className="row justify-content-center">
+                  <div className="col-2">{iconFacebook}</div>
+                  <div className="col-2">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </div>
+                  <div className="col-2">
+                    <FontAwesomeIcon icon={faGooglePlusG} />
+                  </div>
+                  <div className="col-2">
+                    <FontAwesomeIcon icon={faReddit} />
+                  </div>
+                  <div className="col-2">
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                  </div>
+                  <div className="col-2">
+                    <FontAwesomeIcon icon={faFacebookMessenger} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Nav Link */}
+            <div className="container">
+              <ul className="nav nav-tabs">
+                {/* About Place */}
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className={`${tabSelect === 1 ? "active" : ""} nav-link`}
+                    role="tab"
+                    data-toggle="tab"
+                    tabIndex="1"
+                    onClick={handleTabSelected}
+                  >
+                    About Place
+                  </a>
+                </li>
+                {/* Reviews */}
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className={`${tabSelect === 2 ? "active" : ""} nav-link`}
+                    role="tab"
+                    data-toggle="tab"
+                    tabIndex="2"
+                    onClick={handleTabSelected}
+                  >
+                    Reviews
+                  </a>
+                </li>
+                {/* Maps */}
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className={`${tabSelect === 3 ? "active" : ""} nav-link`}
+                    role="tab"
+                    data-toggle="tab"
+                    tabIndex="3"
+                    onClick={handleTabSelected}
+                  >
+                    Maps
+                  </a>
+                </li>
+              </ul>
+              {/* Tab Panel */}
+              <div className="tab-content mb-5">
+                {/* About Place Tab*/}
+                <div
+                  id="aboutPlace"
+                  className={`${
+                    tabSelect !== 1 ? "sr-only" : "active"
+                  } tab-pane in text-center mt-5`}
+                  role="tabpanel"
+                >
+                  <h2 className="mb-3">{title}</h2>
+                  <p>{description}</p>
+                  <img src={img} alt={title} className="img-thumbnail img-fluid" />
+                </div>
+                {/* Reviews */}
+                <div
+                  id="reviews"
+                  className={`${tabSelect !== 2 ? "sr-only" : "active"} tab-pane mt-5`}
+                  role="tabpanel"
+                >
+                  <h2>Reviews content</h2>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum,
+                    veniam ullam aliquid non, maxime ducimus tempora neque voluptates
+                    necessitatibus eligendi sed unde est porro iure voluptatibus pariatur
+                    corrupti rerum fuga.
+                  </p>
+                </div>
+                {/* Maps */}
+                <div
+                  id="maps"
+                  className={`${tabSelect !== 3 ? "sr-only" : "active"} tab-pane mt-5`}
+                  role="tabpanel"
+                >
+                  <iframe
+                    src={maps}
+                    style={{
+                      border: "0",
+                      height: "28.125rem",
+                      width: "100%",
+                      frameborder: "0",
+                    }}
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      }}
+    </InfoConsumer>
   );
 };
 
--- src/data.js
+++ src/data.js
@@ -83,35 +83,35 @@ export const reviews = [
   {
     id: 1,
     name: "John",
-    avatar: ".././avatars/avatar-1.png",
+    avatar: ".././assets/avatars/avatar-1.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 2,
     name: "Mike",
-    avatar: ".././avatars/avatar-2.png",
+    avatar: ".././assets/avatars/avatar-2.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 3,
     name: "Sofia",
-    avatar: ".././avatars/avatar-3.png",
+    avatar: ".././assets/avatars/avatar-3.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 4,
     name: "Joseph",
-    avatar: ".././avatars/avatar-4.png",
+    avatar: ".././assets/avatars/avatar-4.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
   {
     id: 5,
     name: "Natasha",
-    avatar: ".././avatars/avatar-5.png",
+    avatar: ".././assets/avatars/avatar-5.png",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quaerat repellendus. Ad at sit laboriosam sint ea mollitia incidunt voluptatibus inventore libero dolorem velit dolor nobis odit tempora, ex consequuntur.",
   },
@@ -126,7 +126,7 @@ export const detailInfo = {
   title: "Molestias officia excepturi",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?",
-  img: ".././images/garaj.jpg",
+  img: ".././assets/images/garaj.jpg",
   maps:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711",
 };
